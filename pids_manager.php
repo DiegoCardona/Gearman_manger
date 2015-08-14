86cb3ca18d1c61d2af8c93d5cbf667a9a64219e0
Diego Cardona <dcardona@clickdelivery.com>
2015-08-14 17:46:33 -0500

    Se ajusta clase pids_manager

--- pids_manager.php
+++ pids_manager.php
@@ -1,24 +1,134 @@
 <?php
 	
+/**
+ * Class to define an object to administrate php process, the  most important utility is kill and keep tracked the
+ * the php process to save memory and evade cpu issues
+ * @author Diego Cardona <cardona.root@gmail.com>
+ */
 class pids_manager{
 	
-	public $pids = array();
+	private $pids = array();
 
-	public function add( $pid ){
+/**
+ * function set
+ *
+ * Setter that allow int var or int array, to add to the collection
+ * 
+ * @access public
+ * @param pid, int var or int array that represents the process identifier
+ */
+	public function set( $pid ){
 
+		if( is_array( $pid ) ){
+			foreach ($pid as $_pid) {
+				$this->add_pid( $_pid );	
+			}
+		}else{
+			$this->add_pid( $pid );
+		}
 	}
 
+/**
+ * function remove
+ *
+ * Delete an element
+ * 
+ * @access public
+ * @param pid, int var  that represents the process identifier to remove
+ */
+	public function remove( $pid ){
+
+		try{
+			$pointer = array_search( $pid, $this->pids );
+			if( !$pointer ){
+				return FALSE;
+			}
+			unset( $this->pids[$pointer] );
+			return TRUE;
+		}catch( Exception $e ){
+			return FALSE;
+		}
+	}
+
+/**
+ * function get
+ *
+ * Getter that that returns the full collection
+ * 
+ * @access public
+ */
+	public function get(){
+
+		return $this->pids();
+	}
+
+/**
+ * function view
+ *
+ * Show the information of an especific process
+ * 
+ * @access public
+ * @param pointer, the pid key in the collection
+ */
+	public function view( $pointer ){
+
+		$pid = $this->pids[$pointer];
+		return exec( "ps -p ". $pid );
+	}
+
+/**
+ * function kill
+ *
+ * Pids killer, kill a single process
+ * 
+ * @access public
+ * @param pointer, the pid key in the collection
+ */
 	public function kill( $pointer ){
 
+		if( isset( $this->pids[ $pointer ] ) ){
+			return $this->kill_pid( $this->pids[ $pointer ] );
+		}else{
+			return FALSE;
+		}
 	}
 
 	public function kill_all(){
 
+		foreach ($this->pids as $key => $value) {
+			$this->kill( $key );
+		}
+		return TRUE;
 	}
 
-	public function view(){
-
+	private function kill_pid( $pid ){
+		
+		if( !is_null( $pid ) && $pid != '' && !is_nan( $pid ) ){
+			try{
+				system( "kill -9 ". $pid );
+				$this->remove( $pid );
+			}catch( Exception $e ){
+				return FALSE;
+			}
+			return TRUE;
+		}
+		else{
+			return FALSE;
+		}
 	}
+
+	private function add_pid( $pid ){
+
+		if( !is_nan( $pid ) ){
+			array_push( $this->pids, $pid);
+			$pointer = array_search( $pid, $this->pids );
+			$info = $this->view( $pointer );
+			if( empty( $info ) || is_null( $info ) ){
+				unset( $this->pids[$pid] );
+			}
+		}
+	}
+
 }
 
 ?>
--- test_client.php
+++ test_client.php
@@ -1,65 +1,33 @@
 <?php
+	include('pids_manager.php');
 
-$gmclient= new GearmanClient();
-$gmclient->addServer();
-//$gmclient->setCompleteCallback("reverse_complete");
-$gmclient->setCompleteCallback("same_complete");
+	$pids = new pids_manager();
+	$client = new GearmanClient();
+	$results = new StdClass();
 
-$results = new StdClass();
-$results->value = array();
-$results->pids = array();
-$to_send = array(
-	'key1' => 'value',
-	'key2' => 'value',
-	'key3' => 'value',
-	'key4' => 'value'
-);
+	$client->addServer();
+	$client->setCompleteCallback("task_complete");
+	
+	$results->value = array();
+	$results->pids = array();
 
-$to_send = serialize( $to_send );
+	echo "Sending job\n";
 
-echo "Sending job\n";
+	$client->addTask("reverse", "Hello!", $results, "t1");
+	$client->addTask("reverse", "Hello!", $results, "t2");
+	$client->runTasks();
 
-
-$gmclient->addTask("reverse", "Hello!", $results, "t1");
-$gmclient->addTask("reverse", "Hello!", $results, "t2");
-$gmclient->addTask("same", $to_send, $results, "t3");
-
-$gmclient->runTasks();
-
-kill_pids( $results->pids );
+	$pids->add( $results->pids );
+	$pids->kill_all();
 
 
-// function reverse_complete( $task, $results){
+	function task_complete( $task, $results ){
 
-// 		$data = unserialize( $task->data() );
-// 		$results->value[$task->unique()] = array(
-// 				"handle" => $task->jobHandle(),
-// 				"data" => $data['data']
-// 		);
-// 		$results->pid[] = $data['pid'];
-// }
-
-function same_complete( $task, $results ){
-
-	$data = unserialize( $task->data() );
-	if( !in_array( $data['pid'], $results->pids ) ){
-		array_push( $results->pids, $data['pid'] );
+		$data = unserialize( $task->data() );
+		if( !in_array( $data['pid'], $results->pids ) ){
+			array_push( $results->pids, $data['pid'] );
+		}
+		print_r( $data['data'] );
 	}
-	print_r( $data['data'] );
-	echo $data['pid'];
-}
-
-
-function kill_pids( $pids ){
-	print_r( $pids );
-	foreach ($pids as $pid) {
-		if( !is_null( $pid ) && $pid != '' && !is_nan( $pid ) ){
-			echo $pid . "\n";
-			system( 'kill -9 '. $pid );
-		}	
-	}
-}
-
-
 
 ?>
